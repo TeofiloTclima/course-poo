135a01351355b3580642180df40106ddb5185447
TeofiloTclima <teofilotclima@gmail.com>
2022-11-23 20:23:38 -0300

    first commit

---
+++ src/aplication/Program.java
@@ -0,0 +1,30 @@
+package aplication;
+
+import entitis.Products;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Products products = new Products();
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        products.name = sc.nextLine();
+
+        System.out.print("Price: ");
+        products.price = sc.nextDouble();
+
+        System.out.print("Quantity in Stock: ");
+        products.quantity = sc.nextInt();
+
+        System.out.println(products.name + " , " + products.price + " , " + products.quantity  );
+
+        sc.close();
+    }
+}
